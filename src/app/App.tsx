7b8505b49b63a88016df5bd281198db5d3eab6c7
Papperla <krivman@Savar.local>
2023-11-10 13:10:02 +0200

    checkbox added

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,4 +1,19 @@
+import { useState } from 'react'
+
+import { Checkbox } from '@/components/ui/checkbox'
+
 function App() {
-  return <div style={{ padding: '15px' }}></div>
+  const [state, setState] = useState(true)
+
+  return (
+    <div style={{ padding: '15px' }}>
+      <Checkbox
+        checked={state}
+        disabled
+        label={'click here'}
+        onChangeChecked={() => setState(p => !p)}
+      />
+    </div>
+  )
 }
 export default App
---
+++ src/components/ui/checkbox/checkbox.tsx
@@ -0,0 +1,47 @@
+import { CheckBoxIcon } from '@/assets/icons/checkbox-icon'
+import { UnCheckBoxIcon } from '@/assets/icons/uncheckbox-icon'
+import { Typography } from '@/components/ui/typography'
+import * as RadixCheckbox from '@radix-ui/react-checkbox'
+import { clsx } from 'clsx'
+
+import s from './checkbox.module.scss'
+export const Checkbox = ({ checked, className, disabled, label, onChangeChecked }: PropsType) => {
+  const classNames = {
+    container: clsx(s.container),
+    icon_wrapper: clsx(s.icon_wrapper),
+    indicator: clsx(s.CheckboxIndicator),
+    label: clsx(s.Label),
+    root: clsx(s.CheckboxRoot, className),
+  }
+
+  return (
+    <div className={classNames.container}>
+      <RadixCheckbox.Root
+        className={classNames.root}
+        defaultChecked
+        disabled={disabled}
+        id={'c1'}
+        onCheckedChange={onChangeChecked}
+      >
+        <RadixCheckbox.Indicator asChild className={classNames.indicator} forceMount>
+          <div className={classNames.icon_wrapper}>
+            {checked ? <CheckBoxIcon /> : <UnCheckBoxIcon />}{' '}
+          </div>
+        </RadixCheckbox.Indicator>
+      </RadixCheckbox.Root>
+      {label && (
+        <Typography as={'label'} className={classNames.label} htmlFor={'c1'} variant={'body2'}>
+          {label}
+        </Typography>
+      )}
+    </div>
+  )
+}
+
+type PropsType = {
+  checked: boolean
+  className?: string
+  disabled?: boolean
+  label?: string
+  onChangeChecked: () => void
+}
